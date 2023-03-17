092505b1b7e4f46eba70c7d2cac0089d2fa84628
britdannen <britdannen3@gmail.com>
2023-03-17 01:02:49 -0700

    Domino, Player, and Copy Constructor

---
+++ app/src/test/java/GameFramework/Domino.java
@@ -0,0 +1,32 @@
+package GameFramework;
+
+public class Domino {
+    private int end1;
+    private int end2;
+
+    public Domino(int end1, int end2) {
+        this.end1 = end1;
+        this.end2 = end2;
+    }
+
+    public int getEnd1() {
+        return end1;
+    }
+
+    public int getEnd2() {
+        return end2;
+    }
+
+    public void setEnd1(int end1) {
+        this.end1 = end1;
+    }
+
+    public void setEnd2(int end2) {
+        this.end2 = end2;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + end1 + "|" + end2 + "]";
+    }
+}
---
+++ app/src/test/java/GameFramework/Player.java
@@ -0,0 +1,53 @@
+package GameFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private String name;
+    private List<Domino> hand;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<Domino>();
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Domino> getHand() {
+        return hand;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void addDominoToHand(Domino domino) {
+        hand.add(domino);
+    }
+
+    public void removeDominoFromHand(Domino domino) {
+        hand.remove(domino);
+    }
+
+    public boolean hasDomino(Domino domino) {
+        return hand.contains(domino);
+    }
+
+    public boolean hasDominoes() {
+        return !hand.isEmpty();
+    }
+
+    public int getHandSize() {
+        return hand.size();
+    }
+}
+
