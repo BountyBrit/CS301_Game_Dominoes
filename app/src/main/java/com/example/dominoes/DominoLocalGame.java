e7e86057ac71957f03a9d78a0cc4153b7c687de3
britdannen <britdannen3@gmail.com>
2023-04-02 15:39:11 -0700

    LocalGame Methods and action classes

--- app/src/main/java/com/example/dominoes/DominoLocalGame.java
+++ app/src/main/java/com/example/dominoes/DominoLocalGame.java
@@ -1,4 +1,78 @@
 package com.example.dominoes;
 
-public class DominoLocalGame extends MainActivity{
+public class DominoLocalGame {
+    private int lastWinner;
+    private int currentWinner;
+    private DominoGameState dgs;
+
+    /**
+     * This ctor creates a new game state
+     */
+    public DominoLocalGame() {
+        this.dgs = new DominoGameState();
+    }
+    protected boolean makeMove(GameAction action) {
+        if (action instanceof DominoPlaceAction){
+            return false;
+        } else if (action instanceof DominoPassAction){
+            return false;
+        }
+    }
+    public boolean placeTile(Tile tile, int side) {
+        if (tile == null || side < 0 || side > 1) {
+            // invalid arguments
+            return false;
+        }
+
+        if (board.isEmpty()) {
+            // placing the first tile, no need to check sides
+            board.add(tile);
+            return true;
+        }
+
+        Tile lastTile = board.getLast();
+        int lastSide = (side == 0) ? lastTile.getRight() : lastTile.getLeft();
+
+        if (lastSide != tile.getLeft() && lastSide != tile.getRight()) {
+            // tile cannot be placed
+            return false;
+        }
+
+        if (side == 0) {
+            // place tile on the left side
+            if (lastSide == tile.getLeft()) {
+                tile.flip(); // flip tile to match orientation
+            }
+            board.addFirst(tile);
+        } else {
+            // place tile on the right side
+            if (lastSide == tile.getRight()) {
+                tile.flip(); // flip tile to match orientation
+            }
+            board.addLast(tile);
+        }
+
+        return true;
+    }
+
+
+    public boolean isRoundOver() {
+        return false;
+    }
+
+    public boolean isGameOver() {
+        return false;
+    }
+
+    public void addScore(){
+
+    }
+
+    public boolean isValid(){
+        return false;
+    }
+
+    public void alternatePlayer(){
+
+    }
 }
---
+++ app/src/main/java/com/example/dominoes/DominoPassAction.java
@@ -0,0 +1,10 @@
+package com.example.dominoes;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+public class DominoPassAction extends GameAction{
+    public DominoPassAction(GamePlayer player) {
+        super(player);
+    }
+}
